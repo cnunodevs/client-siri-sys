ca2e026fc1e772f2c187ed5544bd70554c601b1a
lvalencia2 <lvalencia@extreme.com.co>
2023-06-26 09:45:24 -0500

    Configuracion de funcionalidad de CRUD del Voluntarios int

--- src/app/pages/voluntarios-int/voluntarios-int-form/voluntarios-int-form.component.ts
+++ src/app/pages/voluntarios-int/voluntarios-int-form/voluntarios-int-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { VoluntarioInternacionalesColDTO } from 'app/shared/models/voluntario-internacionales-col-dto';
+import { PeticionesService } from 'app/shared/services/peticiones.service';
 
 @Component({
   selector: 'app-voluntarios-int-form',
@@ -8,10 +10,13 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class VoluntariosIntFormComponent implements OnInit {
   formulario: FormGroup;
+  isEdit: boolean = false;
+  dataEdit: VoluntarioInternacionalesColDTO;
 
-  constructor(private formBuilder: FormBuilder) { }
-
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private _peticionesService: PeticionesService
+    ) {
     this.formulario = this.formBuilder.group({
       id: [''],
       nombre: ['', Validators.required],
@@ -25,10 +30,58 @@ export class VoluntariosIntFormComponent implements OnInit {
     })
   }
 
-  enviarFormulario() {
+  ngOnInit() {
+    this.dataEdit = JSON.parse(localStorage.getItem("personalApoyo"));
+    if (this.dataEdit) {
+      this.formulario.controls['nombre'].setValue(this.dataEdit.nombre)
+      this.formulario.controls['objeto'].setValue(this.dataEdit.objeto)
+      this.formulario.controls['fechaInicio'].setValue(new Date(this.dataEdit.fechaInicio))
+      this.formulario.controls['fechaFinal'].setValue(new Date(this.dataEdit.fechaFinal))
+      this.formulario.controls['centroFormacion'].setValue(this.dataEdit.centroFormacion)
+      this.formulario.controls['pais'].setValue(this.dataEdit.pais.nombre)
+      this.formulario.controls['asesor'].setValue(this.dataEdit.asesor.nombre)
+      this.formulario.controls['convenio'].setValue(this.dataEdit.convenio.id)
+
+
+      this.isEdit = true;
+    }
+  }
+
+  async enviarFormulario() {
     if (this.formulario.valid) {
       // Aquí puedes hacer lo que necesites con los datos del formulario
-      console.log(this.formulario.value);
+      const json: VoluntarioInternacionalesColDTO = {
+        nombre: this.formulario.value.objeto,
+        objeto: this.formulario.value.nombre,
+        centroFormacion: this.formulario.value.cargo,
+        fechaInicio: new Date(this.formulario.value.fechaInicio),
+        fechaFinal: new Date(this.formulario.value.fechaFinal),
+        pais: {
+          id: 1
+        },
+        asesor: {
+          id: 1
+        },
+        convenio: {
+          id: 1
+        }
+      }
+      if (this.isEdit) {
+        json['id'] = this.dataEdit.id;
+        await this._peticionesService.postDatos("api/v1/voluntarios-internacionales/create", json)
+          .then(() => {
+            this.formulario.reset();
+            this.isEdit = true;
+            localStorage.removeItem("voluntariosInt")
+          })
+        return;
+      }
+      await this._peticionesService.postDatos("api/v1/voluntarios-internacionales/create", json)
+        .then(() => {
+          this.formulario.reset();
+          this.isEdit = true;
+          localStorage.removeItem("voluntariosInt")
+        })
     }
   }
 }
--- src/app/pages/voluntarios-int/voluntarios-int-list/voluntarios-int-list.component.ts
+++ src/app/pages/voluntarios-int/voluntarios-int-list/voluntarios-int-list.component.ts
@@ -11,6 +11,9 @@ import { PeticionesService } from 'app/shared/services/peticiones.service';
 export class VoluntariosIntListComponent implements OnInit , MetodosCRUD {
   idAprendizBorrar :string | null = null;
   voluntarios: VoluntarioInternacionalesColDTO[];
+  dataSeleccionada: VoluntarioInternacionalesColDTO;
+  path: string = '/admin/voluntarios-int/form-voluntarios-int';
+  rutaEliminar: string = 'api/v1/voluntarios-internacionales/delete/by-id/'; 
 
   constructor(
     private _peticionesService: PeticionesService
@@ -20,12 +23,12 @@ export class VoluntariosIntListComponent implements OnInit , MetodosCRUD {
 
   actualizarSeleccion(elemento: VoluntarioInternacionalesColDTO) {
     this.idAprendizBorrar = elemento.id;
-
+    this.dataSeleccionada = elemento;
   }
 
   async obtenerData() {
     try {
-      this.voluntarios = await this._peticionesService.getDatos<VoluntarioInternacionalesColDTO[]>('api/v1/voluntarios-internacionales/paginate');
+      this.voluntarios = await this._peticionesService.getDatos<VoluntarioInternacionalesColDTO[]>('api/v1/voluntarios-internacionales/list/all');
     } catch (error) {
       console.log(error);
     }
